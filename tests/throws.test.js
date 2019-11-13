5c0bf7c3f17596fa1d4b37517b01302a5a89de5c
Tierney Cyren <hello@bnb.im>
2019-11-13 14:54:01 +0000

    tests: attempt to improve Windows story and test in CI because I can't run it on my machine

--- tests/throws.test.js
+++ tests/throws.test.js
@@ -1,9 +1,11 @@
 const list = require('../exports/list')
 
 describe('test `list` export', () => {
-  test('Test the throw when a directory that doesn\'t exist is passed.', () => {    
+  test('Test the throw when a directory that doesn\'t exist is passed.', () => {
     const fakeDirectoryName = 'some-directory-that-doesnt-exist'
-    const errorMessage = `Passed directory ${process.cwd()}/${fakeDirectoryName} does not exist.`
+    const fakeFullPath = process.cwd() + fakeDirectoryName
+    const errorMessage = `Passed directory ${fakeFullPath} does not exist.`
+    const normalisedPath = fakeFullPath.split(path.sep).join('/')
 
     const throwingList = function() {
       list(`./${fakeDirectoryName}`)
