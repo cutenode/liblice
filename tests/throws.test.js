c06eb12e10130c40b455bad349f5137e98811375
Tierney Cyren <hello@bnb.im>
2019-11-13 12:33:29 +0000

    tests: make error message dynamic in throws.test.js

--- tests/throws.test.js
+++ tests/throws.test.js
@@ -2,10 +2,13 @@ const list = require('../exports/list')
 
 describe('test `list` export', () => {
   test('Test the throw when a directory that doesn\'t exist is passed.', () => {    
+    const fakeDirectoryName = 'some-directory-that-doesnt-exist'
+    const errorMessage = `Passed directory ${process.cwd()}/${fakeDirectoryName} does not exist.`
+
     const throwingList = function() {
-      list('./some-directory-that-doesnt-exist') // this "directory" is hard coded in the line below as well
+      list(`./${fakeDirectoryName}`)
     }
 
-    expect(throwingList).toThrowError(new Error(`Passed directory /Users/cyren/GitHub/liblice/some-directory-that-doesnt-exist does not exist.`))
+    expect(throwingList).toThrowError(new Error(errorMessage))
   })
 })
